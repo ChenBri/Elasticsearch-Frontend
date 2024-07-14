bfd5b296d56fb153a5cf2a0710f9155a23a36218
Chen <chenbrilling@gmail.com>
2024-07-14 14:54:20 +0300

    Updated github button animations

--- src/Components/GithubLink.jsx
+++ src/Components/GithubLink.jsx
@@ -1,7 +1,7 @@
 function GithubLink({ link }) {
     return (
         <a href={link} target="_blank" rel="noreferrer">
-            <div className="border-2 border-black  h-min">
+            <div className="border-2 border-black  h-min hover:invert duration-700">
                 <img src="https://img.shields.io/badge/GitHub-ffffff?style=for-the-badge&logo=github&logoColor=black" alt="Github project" />
             </div>
         </a>
